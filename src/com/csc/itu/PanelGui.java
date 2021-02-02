911c99e6b6b1d8570abf2bcc0f161f003c3b906b
jahanfarha3170 <farhatjahan42@gmail.com>
2021-02-01 20:50:22 -0800

    Snake body and snake moving direction is implemented

--- src/com/csc/itu/PanelGui.java
+++ src/com/csc/itu/PanelGui.java
@@ -11,13 +11,13 @@ public class PanelGui extends JPanel implements ActionListener {
     static final int OBJECT_SIZE = 15;// defines the size of the object
     static final int OBJECTS_COUNT = (SCREEN_WIDTH*SCREEN_HEIGHT)/(OBJECT_SIZE); // defines the number of object to fit in this size
     static final int OBJECTS_DELAY = 75;// higher the delay, slow will be the game
-    final int x[] = new int[OBJECTS_COUNT]; // x co-ordinate
-    final int y[] = new int[OBJECTS_COUNT]; // y co-ordinate
+    final int x[] = new int[OBJECTS_COUNT]; // x co-ordinate for snake movement
+    final int y[] = new int[OBJECTS_COUNT]; // y co-ordinate for snake movement
     int snakeBodyPart = 6;
     int appleEatCount;
     int appleGenerateXaxis;
     int appleGenerateYaxis;
-    char direction = 'R'; // assuming snake start from right direction
+    char snakeMovingDirection = 'R'; // assuming snake start from right direction
     boolean running =false;
     Timer time;
     Random random;
@@ -54,6 +54,18 @@ public class PanelGui extends JPanel implements ActionListener {
         }
         g.setColor(Color.red);
         g.fillOval(appleGenerateXaxis, appleGenerateYaxis,OBJECT_SIZE, OBJECT_SIZE);//Size of Apple
+
+        //Below will draw snake body
+        for(int i=0;i<snakeBodyPart;i++){
+            if(i==0){
+                g.setColor(Color.green);
+            }
+            else{
+                g.setColor(Color.yellow);
+            }
+            g.fillRect(x[i], y[i],OBJECT_SIZE, OBJECT_SIZE);
+        }
+
     }
 
     public void addNewAppleAtDifferentLocation(){
@@ -64,29 +76,102 @@ public class PanelGui extends JPanel implements ActionListener {
     }
 
     public void move(){
+        for(int i=snakeBodyPart; i>0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (snakeMovingDirection){
+            case('R'):
+                x[0] = x[0]+OBJECT_SIZE;
+                break;
+            case('L'):
+                x[0] = x[0]-OBJECT_SIZE;
+                break;
+            case('U'):
+                y[0] = y[0]-OBJECT_SIZE;
+                break;
+            case('D'):
+                y[0] = y[0]+OBJECT_SIZE;
+                break;
+        }
 
     }
     public void checkApple(){
 
     }
+
     public void checkCollision(){
+        // Here we will check snake head is collide with snake body
+        for(int i=snakeBodyPart; i>0;i--){
+            if((x[0]==x[i]) && (y[0]==y[i]) ){
+                running=false;
+            }
+        }
+        // Here we will check snake head is collide with left border of game area
+        if(x[0] < 0){
+            running=false;
+        }
+        // Here we will check snake head is collide with right border of game area
+        if(x[0] > SCREEN_WIDTH){
+            running=false;
+        }
+        // Here we will check snake head is collide with Top border of game area
+        if(y[0] <0){
+            running=false;
+        }
+        // Here we will check snake head is collide with Bottom border of game area
+        if(y[0] > SCREEN_HEIGHT){
+            running=false;
+        }
+        if(!running){
+            time.stop();
+        }
 
     }
-    public void actionPerformed(){
 
-    }
     public void GameOver(){
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(running){
+            move();
+            checkApple();
+            checkCollision();
+        }
+        repaint();
     }
 
-    public class  MyKeyAdapter extends KeyAdapter implements KeyListener {
-//        @Override
-//        public void KeyPressed(KeyEvent e){
-//        }
+    public class  MyKeyAdapter extends KeyAdapter {
+        //Here we will limit the snake turn direction to 90 degree limit
+        @Override
+        public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_RIGHT:
+                if (snakeMovingDirection != 'L') {
+                    snakeMovingDirection = 'R';
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (snakeMovingDirection != 'R') {
+                    snakeMovingDirection = 'L';
+                }
+                break;
+            case KeyEvent.VK_UP:
+                if (snakeMovingDirection != 'D') {
+                    snakeMovingDirection = 'U';
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if (snakeMovingDirection != 'U') {
+                    snakeMovingDirection = 'D';
+                }
+                break;
+        }
+        }
+
+        }
 
     }
-}
+
 
